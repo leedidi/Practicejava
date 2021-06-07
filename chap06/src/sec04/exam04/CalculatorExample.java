6b9b5fe0fbe97451d87d64d428e0f99da8229316
leedidi <edy9527@gmail.com>
2021-06-07 23:45:04 +0900

    6/7 Java

---
+++ chap06/src/sec04/exam04/CalculatorExample.java
@@ -0,0 +1,8 @@
+package sec04.exam04;
+
+public class CalculatorExample {
+	public static void main(String[] args) {
+		Calculator myCalc = new Calculator();
+		myCalc.execute(); // Calculator의 execute 메소드 호출
+	}
+}
