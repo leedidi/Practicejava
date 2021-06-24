4fc7cf32a74758c4b7c5ca6419ab64e67d91a465
leedidi <edy9527@gmail.com>
2021-06-24 23:53:37 +0900

    6/24 Java

---
+++ chap11/src/sec01/exam11/KeyboardToStringExample.java
@@ -0,0 +1,13 @@
+package sec01.exam11;
+
+public class KeyboardToStringExample {
+	public static void main(String[] args) throws IOException {
+		byte[] bytes = new byte[100];
+		
+		System.out.print("ют╥б: ");
+		int readByteNo = System.out.read(bytes);
+		
+		String str = new String(bytes, 0, readByteNo-2);
+		System.out.println(str);
+	}
+}
