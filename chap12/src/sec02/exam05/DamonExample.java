54e50b7d3ba32eff2f76435595c1d8290b63763f
leedidi <edy9527@gmail.com>
2021-06-30 23:14:55 +0900

    6/30 Java

---
+++ chap12/src/sec02/exam05/DamonExample.java
@@ -0,0 +1,16 @@
+package sec02.exam05;
+
+public class DamonExample {
+	public static void main(String[] args) {
+		AutoSaveThread autoSaveThread = new AutoSaveThread();
+		autoSaveThread.setDaemon(ture);
+		autoSaveThread.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+	}
+		
+		System.out.println("메인 스레드 종료");
+}
+}
