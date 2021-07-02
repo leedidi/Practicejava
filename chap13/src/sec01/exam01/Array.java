8bbae00f4c69fd57e0fa52f48c1b71f5ce81485a
leedidi <edy9527@gmail.com>
2021-07-02 13:54:53 +0900

    7/2 Java

---
+++ chap13/src/sec01/exam01/Array.java
@@ -0,0 +1,38 @@
+package sec01.exam01;
+
+import java.util.*;
+
+public class Array {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+		
+		list.add("Java");
+		list.add("JBDC");
+		list.add("Servelt/JSP");
+		list.add(2, "Database");
+		list.add("iBATIS");
+		
+		int size = list.size();
+		System.out.println("ÃÑ °´Ã¼¼ö: " + size);
+		System.out.println();   
+		
+		String skill = list.get(2);
+		System.out.println("2: " + skill);
+		System.out.println();
+		
+		for(int i=0;, i<list.size(); i++) {
+			String string = list.get(i);
+			System.out.println(+ ":" + str);
+			}
+		System.out.println();
+		
+		list.remove(2);
+		list.remove(2);
+		list.remove("iBATIS");
+		
+		for(int i=0; i<list.size(); i++) {
+			String string = list.get(i);
+			System.out.println(i + ":" + str);
+		}
+	}
+}
